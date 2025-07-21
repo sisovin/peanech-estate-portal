e8acf9f3ec20dc08f181461fe57040e600235ea6
Your Name <you@example.com>
2025-07-21 19:53:20 +0700

    Update modified: index.html package-lock.json vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,12 @@ export default defineConfig({
   },
   server: {
     // @ts-ignore
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: 'localhost',
+    },
     allowedHosts: true,
   }
 });
